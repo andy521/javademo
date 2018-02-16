e7224cb98666636da14dcd174261c32f6f2c90b6
toutou <datou431@126.com>
2018-02-16 18:27:07 +0800

    大年初一头一回，狗年大吉。

--- sql.sql
+++ sql.sql
@@ -5,5 +5,25 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;*/
 
+/*DROP TABLE IF EXISTS `user1`;
+CREATE TABLE `user1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `sex` bit NOT NULL,
+  `createtime` datetime NOT NULL,
+  primary key (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+insert into user1(name, sex, createtime) value('小赵',1,now());
+insert into user1(name, sex, createtime) value('小钱',0,now());
+insert into user1(name, sex, createtime) value('小孙',0,now());
+insert into user1(name, sex, createtime) value('小李',1,now());
+insert into user1(name, sex, createtime) value('小周',0,now());
+insert into user1(name, sex, createtime) value('小吴',0,now());
+insert into user1(name, sex, createtime) value('小郑',1,now());
+insert into user1(name, sex, createtime) value('小王',0,now());
+*/
 use mytest;
-select * from user;
+select * from user1;
+
+
---
+++ src/main/java/com/csdnspringmvc/service/User1Service.java
@@ -0,0 +1,19 @@
+package com.csdnspringmvc.service;
+
+import com.csdnspringmvc.dao.User1Mapper;
+import com.csdnspringmvc.entity.User1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by toutou on 2018/2/16.
+ */
+@Service
+public class User1Service {
+    @Autowired
+    private User1Mapper userMapper;
+
+    public int insert(User1 user) {
+        return userMapper.insertSelective(user);
+    }
+}
